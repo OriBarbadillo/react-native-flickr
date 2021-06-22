d0140f9cf11153c94a228f27c98b5d191e7ffcb1
OriBarbadillo <oribarbadillo@gmail.com>
2021-06-22 20:42:06 -0300

    Componentes funcionales
    Se pasaron los componentes de clases a funcionales y de ScrollView a FlatList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,45 +1,44 @@
-import React, {Component} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, FlatList} from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
-class AlbumList extends Component {
-  state = {photoset: null};
+const AlbumList = (props) => {
+  //state = {photoset: null};
+  const [photoset, setPhotoSet] = useState(null)
 
-  componentWillMount() {
-    axios
+  useEffect(() => {
+    const fetchPhotoset = async () =>{
+      const { data } = await axios
       .get(
         'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1',
-      )
-      .then((response) =>
-        this.setState({photoset: response.data.photosets.photoset}),
       );
-  }
+      setPhotoSet(data.photosets.photoset);
+    }
+    fetchPhotoset();
+  },[]);
 
-  renderAlbums() {
-    return this.state.photoset.map((album) => (
+  const renderAlbums = (album) => (
       <AlbumDetail
-        navigation={this.props.navigation}
+        navigation={props.navigation}
         key={album.id}
         title={album.title._content}
         albumId={album.id}
       />
-    ));
+  )
+
+  //console.log(this.state);
+
+  if (!photoset) {
+    return <Text>Loading...</Text>;
   }
 
-  render() {
-    console.log(this.state);
-
-    if (!this.state.photoset) {
-      return <Text>Loading...</Text>;
-    }
-
-    return (
-      <View style={{flex: 1}}>
-        <ScrollView>{this.renderAlbums()}</ScrollView>
-      </View>
-    );
-  }
+  return (
+    <View style={{flex: 1}}>
+      <FlatList data={photoset}
+      renderItem={({item}) => renderAlbums(item)}/>
+    </View>
+  );
 }
 
 export default AlbumList;
--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,48 +1,46 @@
-import React, {Component} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ScrollView, Text, View, FlatList} from 'react-native';
 import axios from 'axios';
 import PhotoDetail from './PhotoDetail';
 
-class PhotoList extends Component {
-  state = {photos: null};
-
-  componentWillMount() {
-    axios
+const PhotoList = (props) => {
+  const [photos, setPhotos] = useState(null)
+    
+  useEffect(() => {
+    const fetchPhotos = async () =>{
+      const { data } = await axios
       .get(
-        `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${this.props.route.params.albumId}&user_id=137290658%40N08&format=json&nojsoncallback=1`,
-      )
-      .then((response) =>
-        this.setState({photos: response.data.photoset.photo}),
+        `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${props.route.params.albumId}&user_id=137290658%40N08&format=json&nojsoncallback=1`,
       );
-  }
-
-  renderAlbums() {
-    return this.state.photos.map((photo) => (
-      <PhotoDetail
-        key={photo.title}
-        title={photo.title}
-        imageUrl={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-      />
-    ));
-  }
-
-  render() {
-    console.log(this.state);
-
-    if (!this.state.photos) {
-      return (
-        <View style={{flex: 1}}>
-          <Text>Loading...</Text>
-        </View>
-      );
+      setPhotos(data.photoset.photo);
     }
+    fetchPhotos();
+  },[]);
 
+  const renderAlbums = (photo) => (
+    <PhotoDetail
+     key={photo.title}
+     title={photo.title}
+     imageUrl={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+   />
+ )  
+
+
+  if (!photos) {
     return (
       <View style={{flex: 1}}>
-        <ScrollView>{this.renderAlbums()}</ScrollView>
+        <Text>Loading...</Text>
       </View>
     );
   }
+
+  return (
+    <View style={{flex: 1}}>
+      <FlatList data={photos}
+      renderItem={({item}) => renderAlbums(item)}/>
+    </View>
+  );
+  
 }
 
 export default PhotoList;
